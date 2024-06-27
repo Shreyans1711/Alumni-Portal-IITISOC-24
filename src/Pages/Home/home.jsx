d931af352ec91a84b11bd158c75d53e885a8e12b
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-27 16:28:48 +0530

    Updated pages and their locations

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -4,9 +4,13 @@ import Footer from "../../Components/footer";
 function Home() {
   return (
     <>
-      <Header />
-      <h1 class="text-5xl mb-2">This is home page</h1>
-      <Footer />
+      <div className="header top-0">
+        <Header/>
+      </div>
+      <h1 class="text-5xl">This is home page</h1>
+      <div className="footer absolute bottom-0">
+        <Footer/>
+      </div>
     </>
   );
 }
