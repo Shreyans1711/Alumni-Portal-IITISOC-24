3798ce87607c8b7d42b13dd7f55b10b3f7396e13
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-27 14:34:05 +0530

    Added footer to home page

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../../Components/header";
-
+import Footer from "../../Components/footer";
 function Home() {
   return (
     <>
       <Header />
       <h1 class="text-5xl">This is home page</h1>
+      <div className="foot absolute bottom-0"> 
+        <Footer/>
+      </div>
     </>
   );
 }
