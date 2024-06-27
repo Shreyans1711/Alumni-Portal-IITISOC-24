5a9e74d5cb24e899c2f8f7eee24ac02ab6c1f2b3
Abhishek A P <abhi2004iiti@gmail.com>
2024-06-27 15:19:49 +0530

    minor changes

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import Header from "../../Components/header";
 import Footer from "../../Components/footer";
-<<<<<<< HEAD
-
-=======
->>>>>>> cd5dbe295343a0c06f96553d5899db5ecd9bff38
 function Home() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <h1 class="text-5xl mb-2">This is home page</h1>
       <Footer />
-=======
-      <h1 class="text-5xl">This is home page</h1>
-      <div className="foot absolute bottom-0"> 
-        <Footer/>
-      </div>
->>>>>>> cd5dbe295343a0c06f96553d5899db5ecd9bff38
     </>
   );
 }
