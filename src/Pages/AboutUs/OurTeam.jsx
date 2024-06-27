a97514ba668315c5f4da7d0426e7e087be5e0c48
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-27 16:21:11 +0530

    Shifted noticeboard and added our team page and removed contactUs page

---
+++ src/Pages/AboutUs/OurTeam.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from "../../Components/header";
+import Footer from "../../Components/footer";
+const OurTeam = () => {
+  return (
+    <>
+    <div>
+      <div className="header top-0">
+        <Header/>
+      </div>
+      <h1 class="text-5xl">Our Team</h1>
+      <div className="footer absolute bottom-0">
+        <Footer/>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default OurTeam
--- src/Pages/pages.jsx
+++ src/Pages/pages.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from "./Home/home";
 import IITIAlumniAssociaton from "./AboutUs/IITIAlumniAssociation";
-import NoticeBoard from "./AboutUs/noticeBoard";
+import NoticeBoard from "./Updates/noticeBoard";
 import Developers from "./AboutUs/developers";
-import ContactUs from "./AboutUs/contactUs";
+import OurTeam from "./AboutUs/OurTeam";
 import LatestEvents from "./Events/latestEvents";
 import Reunions from "./Events/reunions";
 import Chapters from "./Events/chapters";
@@ -24,9 +24,8 @@ function Pages() {
             path="/aboutus/IITIAumniAssociation"
             element={<IITIAlumniAssociaton />}
           />
-          <Route path="/aboutus/noticeBoard" element={<NoticeBoard />} />
           <Route path="/aboutus/developers" element={<Developers />} />
-          <Route path="/aboutus/contactUs" element={<ContactUs />} />
+          <Route path="/aboutus/OurTeam" element={<OurTeam/>} />
           <Route path="/awards/alumniAwards" element={<AlumniAwards />} />
           <Route path="/events/latestEvents" element={<LatestEvents />} />
           <Route path="/events/reunions" element={<Reunions />} />
@@ -38,6 +37,7 @@ function Pages() {
           <Route path="/mentorship/jobs" element={<Jobs />} />
           <Route path="/updates/alumniNews" element={<AlumniNews />} />
           <Route path="/updates/newsletter" element={<NewsLetter />} />
+          <Route path="/updates/noticeBoard" element={<NoticeBoard />} />
         </Routes>
       </Router>
     </>
