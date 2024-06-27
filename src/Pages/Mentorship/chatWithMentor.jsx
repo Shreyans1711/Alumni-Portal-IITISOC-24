cd5dbe295343a0c06f96553d5899db5ecd9bff38
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-27 14:54:54 +0530

    Added Header and footer to all pages

--- src/Pages/Mentorship/chatWithMentor.jsx
+++ src/Pages/Mentorship/chatWithMentor.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import Header from "../../Components/header";
+import Footer from "../../Components/footer";
 function ChatWithMentor() {
   return (
     <>
+      <div className="header top-0">
+        <Header/>
+      </div>
       <h1 class="text-5xl">This is the Chat With Mentor page</h1>
+      <div className="footer absolute bottom-0">
+        <Footer/>
+      </div>
     </>
   );
 }
