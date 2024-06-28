1d53497063e431470c8b4d09b21c6689372409ab
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-29 01:45:14 +0530

    Developers Page Done and created a new folder for info and udpdated its path everywhere as well

--- src/Pages/AboutUs/developers.jsx
+++ src/Pages/AboutUs/developers.jsx
@@ -1,17 +1,59 @@
 import React from "react";
 import Header from "../../Components/header";
 import Footer from "../../Components/footer";
+import DevelopersInfo from "../../Info/developersInfo";
+import boy from "../../assets/unknownboys.jpeg";
+
 function Developers() {
+  const Info = [
+    {
+      photo: boy,
+      name: "Shreyans Kothari",
+      year: "2022-2026",
+      dept: "MEMS",
+    },
+    {
+      photo: boy,
+      name: "Abhishek AP",
+      year: "2022-2026",
+      dept: "MEMS",
+    },
+    {
+      photo: boy,
+      name: "Akarshit Trivedi",
+      year: "2022-2026",
+      dept: "MEMS",
+    },
+    {
+      photo: boy,
+      name: "Aditya Sonkhusre",
+      year: "2022-2026",
+      dept: "MEMS",
+    },
+  ];
+
   return (
-    <>
-      <div className="header top-0">
-        <Header/>
+    <body className="bg-[#90E0EF]">
+      <div className="header top-0 w-full">
+        <Header />
       </div>
-      <h1 class="text-5xl">This is the Developers page</h1>
-      <div className="footer absolute bottom-0">
-        <Footer/>
+      <h1 className="text-3xl sm:text-4xl md:text-5xl text-center font-bold font-serif text-white font-outline-2 mt-3">
+        Developers of the Website
+      </h1>
+      <div className="flex flex-wrap justify-center gap-[200px] mt-5 px-4">
+        {Info.map((info, index) => (
+          <div
+            key={index}
+            className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 p-2 flex flex-col items-center"
+          >
+            <DevelopersInfo {...info} />
+          </div>
+        ))}
       </div>
-    </>
+      <div className="footer w-full mt-10">
+        <Footer />
+      </div>
+    </body>
   );
 }
 
