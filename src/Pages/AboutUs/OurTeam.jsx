d66939cad8263c8862994c86be94c537c7349a72
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-29 03:12:28 +0530

    Our Team done and About Us

--- src/Pages/AboutUs/OurTeam.jsx
+++ src/Pages/AboutUs/OurTeam.jsx
@@ -1,20 +1,92 @@
 import React from 'react'
 import Header from "../../Components/header";
 import Footer from "../../Components/footer";
+import OurTeamInfo from "../../Info/OurTeamInfo";
+import boy from "../../assets/unknownboys.jpeg";
 const OurTeam = () => {
+  const Info = [
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    {
+      photo: boy,
+      name: "XYZ",
+      dept: "XYZ",
+    },
+    
+  ];
+
   return (
     <>
-    <div>
-      <div className="header top-0">
-        <Header/>
-      </div>
-      <h1 class="text-5xl">Our Team</h1>
-      <div className="footer w-full mt-10">
-        <Footer />
-      </div>
-    </div>
+      <body className="bg-[#90E0EF]">
+        <div className="header top-0 mb-2">
+          <Header />
+        </div>
+        <h1 class="text-5xl text-center font-bold font-serif text-white font-outline-2">
+          Our Team
+        </h1>
+        <div className="flex flex-wrap justify-center">
+          {Info.flat().map(OurTeamInfo)}
+        </div>
+
+        <div className="footer">
+          <Footer />
+        </div>
+      </body>
     </>
-  )
+  );
 }
 
 export default OurTeam
