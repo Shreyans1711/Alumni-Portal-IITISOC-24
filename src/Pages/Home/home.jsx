9cd578d938c121508062ccffa8b8e1968ffd44ee
Shreyans1711 <shreyanskothari51@gmail.com>
2024-06-30 14:58:02 +0530

    home page

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import Header from "../../Components/header";
 import Footer from "../../Components/footer";
+import "./home.css"
 
 function Home() {
-  const [data, setData] = React.useState(null);
-  async function getData() {
-    const response = await fetch("http://localhost:3000/api");
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const json = await response.json();
-    console.log(json);
-    setData(json.message);
-  }
-  getData();
+  // const [data, setData] = React.useState(null);
+  // async function getData() {
+  //   const response = await fetch("http://localhost:8000/api");
+  //   if (!response.ok) {
+  //     throw new Error(`Response status: ${response.status}`);
+  //   }
+  //   const json = await response.json();
+  //   console.log(json);
+  //   setData(json.message);
+  // }
+  // getData();
 
   return (
-    <>
+    <body className="relative">
       <div className="header top-0">
         <Header />
       </div>
-      <h1 class="text-5xl">This is home page</h1>
-      <p>{!data ? "Loading..." : data}</p>
-      <div className="footer w-full mt-10">
+      {/* <p>{!data ? "Loading..." : data}</p> */}
+      <div className="body">
+        <h1 className="text-6xl md:text-9xl  pt-20 text-center"> Welcome Home</h1>
+        <div className="outer">
+          <div className="text-xl md:text-4xl p-20 text-center font-semibold text-white">Step into the virtual home of IIT INODRE's global community—a place where the Cardinal spirit thrives beyond the palm-lined campus. Reconnect with classmates, discover opportunities, and help juniors.</div>
+        </div>
+      </div>
+      <div className="footer relative-0">
         <Footer />
       </div>
-    </>
+    </body>
   );
 }
 
